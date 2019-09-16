ba9b641427e1c1c9406e5be099ca5cfc771f4afb
Nathaniel D'Rozario <48631513+nsdrozario@users.noreply.github.com>
2019-09-15 17:40:19 -0700

    Update answer_db.php
    Fixed sign error bug involving greatest common denominator

--- engine/answer_db.php
+++ engine/answer_db.php
@@ -13,9 +13,13 @@ $answer = array(
 "linear-solve-x"=>function() {
     $args = func_get_arg(0);
     $ans = ($args[1] - $args[2])/$args[0];
+    $sign = "";
+    if ($ans < 0) {
+    $sign = "-";
+    }
     $gcd = gcd(($args[1] - $args[2]), $args[0]);
     if (intval($ans) != $ans) {
-        return "\\frac{" . ($args[1] - $args[2]) / $gcd  . "}{" . $args[0] / $gcd . "}";
+        return $sign . "\\frac{" . ($args[1] - $args[2]) / $gcd  . "}{" . $args[0] / $gcd . "}";
     } else {
         return $ans;
     }
