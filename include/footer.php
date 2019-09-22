93d0d1ebf18e939920cefff8208702cbc0e63443
Nathaniel D'Rozario <nsdrozario@gmail.com>
2019-09-21 20:54:46 -0700

    update terms of service

--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,14 @@
 <br/><br/>
 <footer class='text-center' id='footer'>
 <p>© 2019 Nishchal Shukla and Nathaniel D'Rozario</p>
-<p>
+<p class="bold">
+    As of September 21, 2019, 8:51 PM Pacific Daylight Time, the Terms of Service have been changed. 
+    <br/>
+    Continued use of Mathemacure and its services constitutes as agreement of these terms. Please
+    <br/>
+    read these updated terms.
+    <br/>
+    </p>
     <a target="_blank" class='allow footer-link' href="termsofservice.php">Terms of Service</a>
     <a target="_blank" class='allow footer-link' href="privacypolicy.php">Privacy Policy</a>
     <a target="_blank" class='allow footer-link' href='about.php'>Attributions</a>
