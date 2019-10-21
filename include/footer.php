213c5849d0611d82c95dfe29a07f15b8a77b336d
Nishchal Shukla <sn362438@kent.k12.wa.us>
2019-10-20 20:57:02 -0700

    Got rid of unecessary breaker tags; spaced out the why section

--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,6 @@
-<br/><br/><br/><br/><br/>
 <footer class='text-center' id='footer'>
     <p class="bold" style="color: red;">
-    As of October 15, 2019, 2:45 PM Pacific Daylight Time, the Privacy Policy has been changed. 
+    As of October 15, 2019, 2:45 PM Pacific Daylight Time, the Privacy Policy has been changed.
     <br/>
     Continued use of Mathemacure and its services after the stated effective date in the Privacy Policy constitutes as agreement of the renewed Privacy Policy. Please
     <br/>
@@ -12,5 +11,4 @@
     <a target="_blank" class='allow footer-link' href="termsofservice.php">Terms of Service</a>
     <a target="_blank" class='allow footer-link' href="privacypolicy.php">Privacy Policy</a>
     <a target="_blank" class='allow footer-link' href='about.php'>Attributions</a>
-</p>
 </footer>
