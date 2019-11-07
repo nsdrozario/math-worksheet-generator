ddaca94f5f2bfc2286b7d7e1c4d12be67b1a2025
Nathaniel D'Rozario <48631513+nsdrozario@users.noreply.github.com>
2019-11-07 12:17:01 -0800

    Update problem_format.php

--- engine/problem_format.php
+++ engine/problem_format.php
@@ -5,7 +5,7 @@
     "add" => " a + a ",
     "sub" => "a - a ",
     "mult" => " a \cdot a ",
-    "divide" => " a / a ",
+    "divide" => " a \div a ",
     "linear-solve-x" => "ax + a = a",
     "quadratic-factoring"=>"ax^2+ax+a=0",
     "parametric-1"=>"(a+at, a+at)"
