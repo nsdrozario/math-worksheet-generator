a2d3076f5695c29fc20e97bb3ff4e1168b9dfe29
RedNova64 <57379272+RedNova64@users.noreply.github.com>
2019-11-08 15:34:56 -0800

    Fixed issues
    Also added white space between sections

--- social_media.php
+++ social_media.php
@@ -21,7 +21,7 @@
 
       <div id='discord'>
          <div class='jumbotron text-center' id='standout'>
-            <h1>id='smaller'>Our Social Media</h1><br/>
+            <h1 id='smaller'>Our Social Media</h1><br/>
             <h2>Get help and information anywhere and anytime.</h2><br/>
          </div>
       </div>
@@ -31,20 +31,17 @@
          <div class="jumbotron text-center">
             <h2>Our Discord</h2>
             <p>Our discord server sounds like it would just be about math information and help, but that is not all that we offer. It is a friendly environment where people are kind to each other and a place to hang out. Its main purpose is to offer people a place to discuss about math or other academic subjects but you can use it to play some games with people or just socialize and talk about life. If you want to join our server. Just click the link below.</p>
-            <br/>
-            <br/>
+            <br/><br/>
             <a target='_blank' class='ghost-btn-mathemacure' href="https://discord.gg/nDd7JCm">Discord Join Link</a>
-            <br/>
-            <br/>
+            <br/><br/><br/><br/>
             <h2>Our Instagram</h2>
             <p>Get the latest information from our website on our instagram in the link below.</p>
-            <br/>
-            <br/>
+            <br/><br/>
             <a target='_blank' class='ghost-btn-mathemacure' href="https://www.instagram.com/mathemacure/">Instagram Link</a>
+            <br/><br/><br/><br/>
             <h2>Our Youtube</h2>
             <p>Having trouble with learning math? Come to our youtube channel to learn complicated math in simple steps. Just click the link below and subscribe to get notifications of our latest videos.</p>
-            <br/>
-            <br/>
+            <br/><br/>
             <a target='_blank' class='ghost-btn-mathemacure' href="https://www.youtube.com/channel/UCOKyYMqO2samm6wPtz4oNuw">Youtube Channel</a>
          </div>
       </div>
