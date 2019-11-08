89874a8f30fc74057067b7fde63c9b1bdf217cde
Sutantu Balamurugan <sutan04@gmail.com>
2019-11-07 17:27:43 -0800

    Finished Discord Page (Needs design updates)
    Just update the design of the website so that it matches the other webpages.

--- discord_info.php
+++ discord_info.php
@@ -7,9 +7,17 @@
     </head>
     <body>
         <?php include 'include/navbar.php'; ?>
-    
-
-
+        <div class="jumbotron text-center" style="color: #FFFFFF;">
+            <h1>Our Discord</h1>
+            <p>Join our discord server for a friendly environment and use it as a forums page where you can ask for help, collaborate, make new friends, and most of all, have fun.</p>
+            <br/>
+            <br/>
+            <h2>What is our Discord?</h2>
+            <p>Our discord server sounds like it would just be about math information and help, but that is not all that we offer. It is a friendly environment where people are kind to each other and a place to hang out. Its main purpose is to offer people a place to discuss about math or other academic subjects but you can use it to play some games with people or just socialize and talk about life. If you want to join our server. Just click the link below.</p>
+            <br/>
+            <br/>
+            <h3>Discord Join Link:</h3>
+            <p>https://discord.gg/nDd7JCm</p> 
         <?php include 'include/cookie_footer.php';
         include 'include/footer.php';
         ?>
