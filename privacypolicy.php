bc887845e60fad6af9bba26ac36950cbd5de5ea0
RedNova64 <57379272+RedNova64@users.noreply.github.com>
2019-11-20 18:00:15 -0800

    Added padding to privacy policy and terms of service

--- privacypolicy.php
+++ privacypolicy.php
@@ -6,52 +6,49 @@
         ?>
     </head>
     <body>
-        <?php 
+        <?php
             include 'include/navbar.php';
         ?>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <h2>Privacy Policy</h2>
-        <h3>I. Introduction</h3>
-<p>Your privacy is important to us. It is Mathemacure's policy to respect your privacy regarding any information we may collect from you across our website, <a class="allow" href="https://mathemacure.me">https://mathemacure.me</a>, and other sites we own and operate.</p>
-<p>We only ask for personal information when we truly need it to provide a service to you. We collect it by fair and lawful means, with your knowledge and consent. We also let you know why we’re collecting it and how it will be used.</p>
-<p>We only retain collected information for as long as necessary to provide you with your requested service. What data we store, we’ll protect within commercially acceptable means to prevent loss and theft, as well as unauthorised access, disclosure, copying, use or modification.</p>
-<p>We don’t share any personally identifying information publicly or with third-parties, except when required to by law.</p>
-<p>Our website may link to external sites that are not operated by us. Please be aware that we have no control over the content and practices of these sites, and cannot accept responsibility or liability for their respective privacy policies.</p>
-<p>You are free to refuse our request for your personal information, with the understanding that we may be unable to provide you with some of your desired services.</p>
-<br/>
-<p>This document will now elaborate in further detail about how we use your data.</p>
-<br/>
-<h3>II. Information we Collect</h3>
-<p>Mathemacure does not actually collect any personal data.</p>
-<br/>
-<p>The only data that Mathemacure collects is the data inputted into our worksheet generator, which is used to generate math worksheets and answer keys.
-<br/>
-Furthermore, Mathemacure does not utilize a database to store any data we collect from you. Thus, the only data stored from you will be on cloud server's random access memory.
-<br/>
-Mathemacure does not send your data to other third parties. However, your data is processed using MathJax, a client-side JavaScript library that runs on your computer, and an extension for that library called forminput. More details can be found at the <a class="allow" href="http://mathjax.org">MathJax website</a> and the <a class="allow" href="https://github.com/mathjax/MathJax-third-party-extensions/tree/master/legacy/forminput">forminput repository page</a>. Please note that Mathemacure is not responsible for the practices, actions or content of links that lead to external websites. Mathemacure is not affiliated with MathJax, forminput, and GitHub in any way; we just use their services for our website.
-Therefore, no data inputted into our website should be kept indefinetly. Cookies, however, follow a different set of rules.
-<br/>
-However, the user is responsible for any input or action that disrupts the Mathemacure system or compromises the security of Mathemacure or any users using Mathemacure and is responsible for providing relevant and usable input into our services. Additionally, due to the lack of collection of personal data in the Mathemacure website, you are not able to change your information.
-</p>
-<h3>III. Cookies</h3>
-<p>In order to provide the best experience for our users, Mathemacure stores information on your computer called 'cookies'. This information is then accessed by our website to personalize your experience. The cookies our website uses are to personalize how mathematical content looks on your browser and to see if you accept our Terms of Service and Privacy Policy, including our use of cookies.
-<br/>
-However, the cookies that are used to personalize your viewing of mathematical content are created using a library called MathJax, and thus we do not know of any potential hidden or obscure uses for cookies for that library; we only know that the client-side JavaScript library MathJax uses cookies to store information about how math is rendered.
-<br/>
-Furthermore, 
-</p><h3>IV. Do Not Track Signals</h3>
-<p>Our website ignores Do Not Track requests. Our website has no need to acknowledge Do Not Track requests to keep your information private. This is because we do not keep information that can be used to identify you and track you, and we do not use any third-party services that can track you. Services like MathJax that do handle your information are run on the client-side, which is run on your computer, not our server. Mathemacure does not utilize advertising services.</p>
-</p>
-<h3>V. Changes to the Privacy Policy</h3>
-<p>Mathemacure's Privacy Policy is subject to modification at any time. Mathemacure will notify users about these privacy policy changes by putting a notice in the footer of most pages of the website, with the exception of pages that are used internally within the system and pages like printable_worksheet.php which is used to print worksheets and answer keys, unlike other pages on our website which are for informative purposes or for users to input data (non-personal, non-identifying data) to our system to be made into worksheets and answer keys. Mathemacure does not guarantee that these notifications will be sent before the changes happen; sometimes due to critical importance Privacy Policy modifications will be effective immediately.</p>
-<h3>VI. Other conditions</h3>
-<p>Your continued use of our website will be regarded as acceptance of our practices around privacy and personal information. If you have any questions about how we handle user data and personal information, feel free to contact us by email at mathemacure@gmail.com.</p>
-<p>This policy is effective as of 15 October 2019 3:00 PM Pacific Daylight Time.</p>
-<p>Generated by <a class="allow" title="Privacy Policy Template Generator" href="https://getterms.io/">GetTerms.io</a></p>
+        <div style="padding: 100px 0 50px; width: 90%; margin: auto;">
+            <h2>Privacy Policy</h2>
+            <h3>I. Introduction</h3>
+            <p>Your privacy is important to us. It is Mathemacure's policy to respect your privacy regarding any information we may collect from you across our website, <a class="allow" href="https://mathemacure.me">https://mathemacure.me</a>, and other sites we own and operate.</p>
+            <p>We only ask for personal information when we truly need it to provide a service to you. We collect it by fair and lawful means, with your knowledge and consent. We also let you know why we’re collecting it and how it will be used.</p>
+            <p>We only retain collected information for as long as necessary to provide you with your requested service. What data we store, we’ll protect within commercially acceptable means to prevent loss and theft, as well as unauthorised access, disclosure, copying, use or modification.</p>
+            <p>We don’t share any personally identifying information publicly or with third-parties, except when required to by law.</p>
+            <p>Our website may link to external sites that are not operated by us. Please be aware that we have no control over the content and practices of these sites, and cannot accept responsibility or liability for their respective privacy policies.</p>
+            <p>You are free to refuse our request for your personal information, with the understanding that we may be unable to provide you with some of your desired services.</p>
+            <br/>
+            <p>This document will now elaborate in further detail about how we use your data.</p>
+            <br/>
+            <h3>II. Information we Collect</h3>
+            <p>Mathemacure does not actually collect any personal data.</p>
+            <br/>
+            <p>The only data that Mathemacure collects is the data inputted into our worksheet generator, which is used to generate math worksheets and answer keys.
+            <br/>
+            Furthermore, Mathemacure does not utilize a database to store any data we collect from you. Thus, the only data stored from you will be on cloud server's random access memory.
+            <br/>
+            Mathemacure does not send your data to other third parties. However, your data is processed using MathJax, a client-side JavaScript library that runs on your computer, and an extension for that library called forminput. More details can be found at the <a class="allow" href="http://mathjax.org">MathJax website</a> and the <a class="allow" href="https://github.com/mathjax/MathJax-third-party-extensions/tree/master/legacy/forminput">forminput repository page</a>. Please note that Mathemacure is not responsible for the practices, actions or content of links that lead to external websites. Mathemacure is not affiliated with MathJax, forminput, and GitHub in any way; we just use their services for our website.
+            Therefore, no data inputted into our website should be kept indefinetly. Cookies, however, follow a different set of rules.
+            <br/>
+            However, the user is responsible for any input or action that disrupts the Mathemacure system or compromises the security of Mathemacure or any users using Mathemacure and is responsible for providing relevant and usable input into our services. Additionally, due to the lack of collection of personal data in the Mathemacure website, you are not able to change your information.
+            </p>
+            <h3>III. Cookies</h3>
+            <p>In order to provide the best experience for our users, Mathemacure stores information on your computer called 'cookies'. This information is then accessed by our website to personalize your experience. The cookies our website uses are to personalize how mathematical content looks on your browser and to see if you accept our Terms of Service and Privacy Policy, including our use of cookies.
+            <br/>
+            However, the cookies that are used to personalize your viewing of mathematical content are created using a library called MathJax, and thus we do not know of any potential hidden or obscure uses for cookies for that library; we only know that the client-side JavaScript library MathJax uses cookies to store information about how math is rendered.
+            <br/>
+            Furthermore,
+            </p><h3>IV. Do Not Track Signals</h3>
+            <p>Our website ignores Do Not Track requests. Our website has no need to acknowledge Do Not Track requests to keep your information private. This is because we do not keep information that can be used to identify you and track you, and we do not use any third-party services that can track you. Services like MathJax that do handle your information are run on the client-side, which is run on your computer, not our server. Mathemacure does not utilize advertising services.</p>
+            </p>
+            <h3>V. Changes to the Privacy Policy</h3>
+            <p>Mathemacure's Privacy Policy is subject to modification at any time. Mathemacure will notify users about these privacy policy changes by putting a notice in the footer of most pages of the website, with the exception of pages that are used internally within the system and pages like printable_worksheet.php which is used to print worksheets and answer keys, unlike other pages on our website which are for informative purposes or for users to input data (non-personal, non-identifying data) to our system to be made into worksheets and answer keys. Mathemacure does not guarantee that these notifications will be sent before the changes happen; sometimes due to critical importance Privacy Policy modifications will be effective immediately.</p>
+            <h3>VI. Other conditions</h3>
+            <p>Your continued use of our website will be regarded as acceptance of our practices around privacy and personal information. If you have any questions about how we handle user data and personal information, feel free to contact us by email at mathemacure@gmail.com.</p>
+            <p>This policy is effective as of 15 October 2019 3:00 PM Pacific Daylight Time.</p>
+            <p>Generated by <a class="allow" title="Privacy Policy Template Generator" href="https://getterms.io/">GetTerms.io</a></p>
+        </div>
         <?php
             include 'include/cookie_footer.php';
             include 'include/footer.php';
